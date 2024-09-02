f0ca062ab26aacc997d31d16cc4b0ecd326a35ef
akhilmohnn <akhilmohnn@outlook.com>
2024-09-02 23:11:55 +0530

    ...

---
+++ Mits Pgms/sizeof.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+    int i=4;
+    float k=5;
+
+    printf("Size of 1st variable is:%zu\n",sizeof(i));
+    printf("Size of 2nd variable is:%zu",sizeof(k));
+
+}
