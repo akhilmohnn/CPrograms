39b17f9034dd65ba6dd04435bebffbbd2c346b64
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2024-10-07 10:57:00 +0530

    Using dynamic memory allocation

---
+++ Mits Pgms/S1 MCA/ds4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Using dynamic memory allocation
+
+struct person{
+char name[20];
+int age;
+float weight;
+};
+
+int main(){
+
+struct person *ptr;
+int i,n;
+
+printf("Enter the number of persons:");
+scanf("%d",&n);
+
+ptr=(struct person*)malloc(n*sizeof(struct person));
+
+for(i=0;i<n;i++)
+{
+printf("Enter the name and age of person");
+scanf("%s%d",(ptr+i)->name,&(ptr+i)->age);
+}
+
+printf("\nDetails:");
+for(i=0;i<n;i++){
+printf("Name :%s\nAge:%d\n",(ptr+i)->name,(ptr+i)->age);
+
+}
+}
+
