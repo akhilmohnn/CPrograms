89d11532e2d6e70e432cbbfe3cf94ac347470456
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2024-10-08 15:42:46 +0530

    read and print n integers using malloc

---
+++ Mits Pgms/S1 MCA/DataStructure/ds5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+int i,n;
+int *arr;
+
+printf("Enter the size of integer count");
+scanf("%d",&n);
+
+arr=(int n*)malloc(n* sizeof(int));
+
+printf("Enter the elements:\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+
+printf("Entered elements are:\n");
+for(i=0;i<n;i++)
+{
+printf("%d\n",arr[i]);
+}
+
+free(arr);
+}
