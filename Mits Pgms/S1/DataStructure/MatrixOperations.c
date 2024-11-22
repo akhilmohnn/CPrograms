befade004df5e21a4811a6d9ba90230a77853288
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-22 22:56:47 +0530

    Matrix operations:add,sub,mul (menu driven)

--- Mits Pgms/S1/DataStructure/MatrixOperations.c
+++ Mits Pgms/S1/DataStructure/MatrixOperations.c
@@ -13,13 +13,49 @@ int readMatrix(int matrix[MAX][MAX],int rows,int cols){
     }
 }
 
-int displayMatrix(int matrix[MAX][MAX],rows,cols){
+int displayMatrix(int matrix[MAX][MAX],int rows,int cols){
     printf("Matrix elements (%d x%d):\n",rows,cols);
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
-            printf("%d",matrix[i][j])
+            printf("%d  ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int addMatrix(int matrix1[MAX][MAX],int matrix2[MAX][MAX],int result[MAX][MAX],int rows,int cols){
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            result[i][j]=matrix1[i][j]+matrix2[i][j];
         }
     }
+    printf("Result after addition:\n");
+    displayMatrix(result,rows,cols);
+}
+
+int subMatrix(int matrix1[MAX][MAX],int matrix2[MAX][MAX],int result[MAX][MAX],int rows,int cols){
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            result[i][j]=matrix1[i][j]-matrix2[i][j];
+        }
+    }
+    printf("Result after substraction:\n");
+    displayMatrix(result,rows,cols);
+}
+
+int multiplyMatrix(int matrix1[MAX][MAX],int matrix2[MAX][MAX],int result[MAX][MAX],int rows1,int rows2,int cols1,int cols2){
+
+for(int i=0;i<rows1;i++){
+    for(int j=0;j<cols2;j++){
+        result[i][j]=0;
+        for(int k=0;k<cols1;k++){
+            result[i][j]+=matrix1[i][k]*matrix2[k][j];
+        }
+    }
+}
+printf("Result of matrix multiplication:\n");
+    displayMatrix(result, rows1, cols2);
+
 }
 
 int main(){
@@ -38,7 +74,7 @@ int main(){
 
 
     while(1){
-        printf("Choose the option:\n");
+        printf("\nChoose the option:\n");
         printf("1.Add matrices\n");
         printf("2.Perform substraction\n");
         printf("3.Multiplication\n");
@@ -47,15 +83,41 @@ int main(){
         scanf("%d",&ch);
 
         switch(ch){
-            case 1: if(rows1==1 && cols1==cols2){
+            case 1: if(rows1==rows2 && cols1==cols2){
+            addMatrix(matrix1,matrix2,result,rows1,cols1);
+            }
+                    else{
+                        printf("Matrix addition not possible, different dimensions!!!\n");
+                    }
 
-                addMatrix(matrix1,matrix2,result,rows1,cols1);
+            break;
+
+            case 2:if(rows1==rows2 && cols1==cols2){
+                subMatrix(matrix1,matrix2,result,rows1,cols1);
+                }
+                else{
+                    printf("Matrix substraction not possible, different dimensions!!!\n");
+                }
+                break;
+
+            case 3: if(cols1==rows2){
+                multiplyMatrix(matrix1,matrix2,result,rows1,rows2,cols1,cols2);
+            }
+            else{
+                printf("Mutiplication not possible, differnt cols. and rows. sizes");
+            }
+            break;
+
+            case 4: printf("Exiting program");
+            return 0;
+
+            default: printf("Invalid,try again!!!");
 
                 
-            }
-            
         }
-    }
+            
+        
+        }
 
 
 
